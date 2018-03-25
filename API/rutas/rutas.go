b2b8c33e8445b63262f9436dd847fc534d45e724
Mariiacristina <mariacristina.binfa@mail.udp.cl>
2018-03-25 14:24:12 -0400

    arreglé los handleFunc del main

--- API/rutas/rutas.go
+++ API/rutas/rutas.go
@@ -17,53 +17,35 @@ func RouteringMarcianos() (http.Handler) {
 	router.HandleFunc("/Marciano/{id}", controller.GetMarciano).Methods("GET")
   //eliminar un marciano
   router.HandleFunc("/Marciano/{id}", controller.DeleteMarciano).Methods("DELETE")
-  return router
-}
 
-func RouteringNaves() (http.Handler) {
-  router_naves := mux.NewRouter()
 ////////////NAVE NODRIZA //////////////////////////////////////////////////////
   //obtener varias naves
-  router_naves.HandleFunc("/Nave", controller.GetNaves).Methods("GET")
+  router.HandleFunc("/Nave", controller.GetNaves).Methods("GET")
   //postear una nave
-  router_naves.HandleFunc("/Nave",controller.PostNave).Methods("POST")
+  router.HandleFunc("/Nave",controller.PostNave).Methods("POST")
   //obtener una nave
-  router_naves.HandleFunc("/Nave/{id}", controller.GetNave).Methods("GET")
+  router.HandleFunc("/Nave/{id}", controller.GetNave).Methods("GET")
   //eliminar una nave
-  router_naves.HandleFunc("/Nave/{id}", controller.DeleteNave).Methods("DELETE")
-  return router_naves
-}
+  router.HandleFunc("/Nave/{id}", controller.DeleteNave).Methods("DELETE")
 
-
-func RouteringAero()(http.Handler){
-router_aero := mux.NewRouter()
 ///////////AERONAVE//////////////////////////////////////////////////////////////
-//obtener varias aeronave
-router_aero.HandleFunc("/Aero", controller.GetAeros).Methods("GET")
-//postear una aeronave
-router_aero.HandleFunc("/Aero", controller.PostAero).Methods("POST")
-//obtener una aeronave
-router_aero.HandleFunc("/Aero/{id}", controller.GetAero).Methods("GET")
-//eliminar una AERONAVE
-router_aero.HandleFunc("/Aero/{id}", controller.DeleteAero).Methods("DELETE")
-return router_aero
-}
+  //obtener varias aeronave
+  router.HandleFunc("/Aero", controller.GetAeros).Methods("GET")
+  //postear una aeronave
+  router.HandleFunc("/Aero", controller.PostAero).Methods("POST")
+  //obtener una aeronave
+  router.HandleFunc("/Aero/{id}", controller.GetAero).Methods("GET")
+  //eliminar una AERONAVE
+  router.HandleFunc("/Aero/{id}", controller.DeleteAero).Methods("DELETE")
 
-func RouteringViaje()(http.Handler){
-  router_viaje := mux.NewRouter()
   /////////////VIAJEEES/////////////////////////////////////////////////////////
   //insert viaje
-  router_viaje.HandleFunc("/Viaje", controller.PostViaje).Methods("POST")
+  router.HandleFunc("/Viaje", controller.PostViaje).Methods("POST")
   //delete Viaje
-  router_viaje.HandleFunc("/Viaje/{id}", controller.PostViaje).Methods("DELETE")
-  return router_viaje
-}
+  router.HandleFunc("/Viaje/{id}", controller.PostViaje).Methods("DELETE")
 
-
-func RouteringRevision()(http.Handler){
   /////////////////REVISION//////////////////////////////////////////////////////
-  router_revision := mux.NewRouter()
   //insert REVISION
-  router_revision.HandleFunc("/Revision", controller.PostRevision).Methods("POST")
-  return router_revision
+  router.HandleFunc("/Revision", controller.PostRevision).Methods("POST")
+  return router
 }
--- cmd/main.go
+++ cmd/main.go
@@ -10,16 +10,4 @@ import (
 func main() {
   m := rutas.RouteringMarcianos()
 	log.Fatal(http.ListenAndServe(":8000",m))
-
-  n := rutas.RouteringNaves()
-  log.Fatal(http.ListenAndServe(":8000",n))
-
-  a := rutas.RouteringAero()
-  log.Fatal(http.ListenAndServe(":8000",a))
-
-  v := rutas.RouteringViaje()
-  log.Fatal(http.ListenAndServe(":8000",v))
-
-  r := rutas.RouteringRevision()
-  log.Fatal(http.ListenAndServe(":8000",r))
 }
